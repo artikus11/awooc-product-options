adbd7da783899c5a4aa5134f67fa440557a71fc1
Артем <artikus.sol@gmail.com>
2025-01-17 21:24:14 +0400

    refactor(awooc_po 1.1.0): ArtWoocommerceProductOptions - удаление лишних вложенностей, исправление ошибок пересчета цены

--- src/Handles/ArtWoocommerceProductOptions.php
+++ src/Handles/ArtWoocommerceProductOptions.php
@@ -36,15 +36,14 @@ class ArtWoocommerceProductOptions extends Handle {
 
 		$product = wc_get_product( $product_id );
 
-		$post_data          = map_deep( wp_unslash( (array) $_POST['awpo_option'] ), 'sanitize_text_field' );
-		$post_data_quantity = ! empty( $_POST['quantity'] ) ? sanitize_text_field( wp_unslash( (int) $_POST['quantity'] ) ) : 1;
+		$post_data = map_deep( wp_unslash( (array) $_POST['awpo_option'] ), 'sanitize_text_field' );
 		// phpcs:enable WordPress.Security.NonceVerification.Missing
 
 		$options_data = $this->awpo_main->get_cart()->prepared_selected_data( $product->get_id(), $post_data );
 
 		$options['options']  = $options_data;
 		$options['amount']   = $this->get_total_options( $options_data, $product );
-		$options['quantity'] = $post_data_quantity;
+		$options['quantity'] = $this->get_quantity();
 
 		return $options;
 	}
@@ -81,9 +80,9 @@ class ArtWoocommerceProductOptions extends Handle {
 				$item->set_total( $product_price_with_options );
 
 				$item->save();
-
-				$order_total += $item->get_total() + $item->get_total_tax();
 			}
+
+			$order_total += $item->get_total() + $item->get_total_tax();
 		}
 
 		$discount_total = $order->get_discount_total();
@@ -94,34 +93,5 @@ class ArtWoocommerceProductOptions extends Handle {
 
 		$order->set_total( $order_total );
 		$order->save();
-	}
-
-
-	/**
-	 * @param  array      $options_data
-	 * @param  WC_Product $product
-	 *
-	 * @return float
-	 */
-	protected function get_total_options( array $options_data, WC_Product $product ): float {
-
-		$options_total = 0;
-		$amount        = 0;
-
-		foreach ( $options_data as $field ) {
-			if ( empty( $field['value'] ) ) {
-				continue;
-			}
-
-			$current_price = $product->get_price();
-
-			if ( ! empty( $field['price'] ) ) {
-				$options_total += $field['price'];
-			}
-
-			$amount = (float) $options_total + (float) $current_price;
-		}
-
-		return $amount;
 	}
 }
